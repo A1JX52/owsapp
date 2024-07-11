a10c68af68fc7a0173b4274233c0020cfe34cf00
flo <florian.eigendorf@tum.de>
2024-07-11 18:14:53 +0200

    add start/stop functionality

--- components/DevView.tsx
+++ components/DevView.tsx
@@ -10,9 +10,13 @@ import {useDatabase} from '../contexts/dbContext';
 
 const DevView = () => {
   const [acc, setAcc] = useState<AccelerometerItem>({x: 0, y: 0, z: 0});
+  const [record, setRecord] = useState(false);
   const db = useDatabase();
 
   useEffect(() => {
+    if (!record) {
+      return;
+    }
     setUpdateIntervalForType(SensorTypes.accelerometer, 200);
 
     const sub = accelerometer.subscribe({
@@ -20,19 +24,18 @@ const DevView = () => {
       error: (error: any) => console.error('reading sensor data failed', error),
     });
     return () => sub.unsubscribe();
-  }, []);
+  }, [record]);
     
   return (
     <View>
+      <Button title={record ? 'stop' : 'start'} onPress={() => setRecord(!record)}/>
       <Text style={styles.txt}>{JSON.stringify(acc)}</Text>
-      <Button title="get latest item" onPress={async () => {
+      <Button title='get latest item' onPress={async () => {
         let acc = await db.getAcc();
-        if (acc) {
-          setAcc(acc);
-        }
+        setAcc(acc ? acc : {x: 0, y: 0, z: 0});
       }}/>
       <Button
-        title="delete all items"
+        title='delete all items'
         onPress={() => {
           Alert.alert('confirm', 'are you sure you want to delete all items?', [
             {text: 'cancel'},
