3c5ebb4a16620b62c0c5f0acb71bff3e880f1eb8
flo <florian.eigendorf@tum.de>
2024-07-30 15:35:37 +0200

    migrate chart to raw skia using d3

--- components/FrequencyChart.tsx
+++ components/FrequencyChart.tsx
@@ -1,58 +1,99 @@
-import React, { useState , useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { GraphPoint, LineGraph } from 'react-native-graph';
+import React, { useState, useMemo } from 'react';
+import { View, StyleSheet, Text, LayoutChangeEvent } from 'react-native';
+import { Skia, Canvas, Group, Path, ColorShader } from '@shopify/react-native-skia';
+import { scaleLinear } from 'd3-scale'
 import { AccelerometerItem } from '../models';
 
+const PADDING = 16;
+
+interface DataPoint {
+  date: number,
+  value: number,
+}
+
+const buildPoints = (items: AccelerometerItem[]): DataPoint[] => {
+  const groupedData: { [key: number]: number } = {};
+
+  items.forEach((item: AccelerometerItem) => {
+    const date = new Date(item.timestamp);
+    date.setMilliseconds(0);
+    const minute = date.getTime();
+
+    if (!groupedData[minute]) {
+      groupedData[minute] = 0;
+    }
+    groupedData[minute]++;
+  });
+
+  return Object.entries(groupedData).map(([timestamp, count]) => ({
+    date: +timestamp,
+    value: count
+  }));
+};
+
 const FrequencyChart = ({ items }: { items: AccelerometerItem[] }) => {
-  const [value, setValue] = useState(-1);
+  const [dimension, setDimension] = useState({width: 0, height: 0});
 
-  const groupDataByMinute = (items: AccelerometerItem[]) => {
-    const groupedData: {[key: number]: number} = {};
-    
-    items.forEach((item: AccelerometerItem) => {
-      const date = new Date(item.timestamp);
-      date.setSeconds(0, 0);
-      const minute = date.getTime();
-      
-      if (!groupedData[minute]) {
-        groupedData[minute] = 0;
-      }
-      groupedData[minute]++;
-    });
-
-    return Object.entries(groupedData).map(([timestamp, count]) => ({
-      date: new Date(Number(timestamp)),
-      value: count
-    }));
+  const handleCanvasLayout = (event: LayoutChangeEvent) => {
+    const { width, height } = event.nativeEvent.layout;
+    setDimension({ width, height });
   };
 
-  const [points, setPoints] = useState<GraphPoint[]>(groupDataByMinute(items));
+  const buildPath = (data: DataPoint[]) => {
+    const path = Skia.Path.Make();
+  
+    if (!data.length) return path;
+  
+    const values = data.map(pt => pt.value);
+    const dates = data.map(pt => pt.date);
+  
+    const xScale = scaleLinear()
+      .domain([Math.min(...dates), Math.max(...dates)])
+      .range([PADDING, dimension.width - PADDING]);
+  
+    const yScale = scaleLinear()
+      .domain([Math.min(...values), Math.max(...values)])
+      .range([dimension.height - PADDING, PADDING]);
+  
+    path.moveTo(xScale(data[0].date), yScale(data[0].value));
+    data.shift();
+  
+    data.forEach(pt => {
+      path.lineTo(xScale(pt.date), yScale(pt.value));
+    });
+    return path;
+  };
 
-  useEffect(() => {
-    setPoints(groupDataByMinute(items));
-  }, [items]);
+  const points = useMemo(() => buildPoints(items), [items, dimension]);
+  const path = useMemo(() => buildPath(points), [points]);
 
   return (
-    <View>
-      <Text style={styles.txt}>{value}</Text>
-      <LineGraph style={styles.cont}
-        points={points}
-        animated={true}
-        color='#4484B2'
-        enablePanGesture={true}
-        onPointSelected={(p) => setValue(p.value)}
-        onGestureEnd={() => setValue(-1)}
-      />
+    <View style={styles.cont}>
+      <Text style={styles.txt}>Frequency Chart</Text>
+      <Canvas style={{ flex: 1 }} onLayout={handleCanvasLayout}>
+        <Group>
+          <Path
+            path={path}
+            style='stroke'
+            strokeWidth={3}
+          >
+            <ColorShader color='lightBlue' />
+          </Path>
+        </Group>
+      </Canvas>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   cont: {
+    backgroundColor: '#1F1D2B',
+    padding: PADDING,
     height: 200,
   },
   txt: {
     fontSize: 20,
+    color: 'white',
   },
 });
 
