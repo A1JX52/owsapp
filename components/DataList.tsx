ed1e373ffecfe030c72e792b9711610325352be6
flo <florian.eigendorf@tum.de>
2024-08-07 19:23:14 +0200

    fix flatlist style

--- components/DataList.tsx
+++ components/DataList.tsx
@@ -6,55 +6,55 @@ import DataListItem from './DataListItem';
 import FrequencyChart from './FrequencyChart';
 
 const DataList = () => {
-    const [loading, setLoading] = useState(false);
-    const [items, setItems] = useState<AccelerometerItem[]>([]);
-    const [nextPage, setNextPage] = useState(0);
-    const [allItems, setAllItems] = useState<AccelerometerItem[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [items, setItems] = useState<AccelerometerItem[]>([]);
+  const [nextPage, setNextPage] = useState(0);
+  const [allItems, setAllItems] = useState<AccelerometerItem[]>([]);
 
-    const db = useDatabase();
-    const limit = 15;
-  
-    const fetchPage = async (page: number) => {
-      if (loading) return;
-      setLoading(true);
+  const db = useDatabase();
+  const limit = 15;
 
-      const newItems = await db.getSubsetAcc(page, limit);
-      
-      setItems((items) => {
-        return [...items, ...newItems]
-      });
-      setNextPage(page + limit);
-      setLoading(false);
-    };
-  
-    useEffect(() => {
-      fetchPage(0);
-    }, []);
-  
-    const onRefresh = () => {
-      setItems([]);
-      (async () => {
-        setAllItems(await db.getSubsetAcc(0, -1));
-      })();
-      fetchPage(0);
-    }
-  
-    return (
-      <View>
-        <FrequencyChart items={allItems.reverse()} />
-        <FlatList
-          data={items}
-          renderItem={({ item }) => <DataListItem item={ item } />}
-          contentContainerStyle={{ gap: 20 }}
-          onEndReached={() => fetchPage(nextPage)}
-          onEndReachedThreshold={1}
-          ListFooterComponent={() => loading && <ActivityIndicator />}
-          refreshing={loading}
-          onRefresh={onRefresh}
-          ListEmptyComponent={<Text>there are no recordings</Text>}
-        />
-      </View>
-    );
+  const fetchPage = async (page: number) => {
+    if (loading) return;
+    setLoading(true);
+
+    const newItems = await db.getSubsetAcc(page, limit);
+    
+    setItems((items) => {
+      return [...items, ...newItems]
+    });
+    setNextPage(page + limit);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPage(0);
+  }, []);
+
+  const onRefresh = () => {
+    setItems([]);
+    (async () => {
+      setAllItems(await db.getSubsetAcc(0, -1));
+    })();
+    fetchPage(0);
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <FrequencyChart items={allItems.reverse()} />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <DataListItem item={ item } />}
+        contentContainerStyle={{ gap: 20 }}
+        onEndReached={() => fetchPage(nextPage)}
+        onEndReachedThreshold={1}
+        ListFooterComponent={() => loading && <ActivityIndicator />}
+        refreshing={loading}
+        onRefresh={onRefresh}
+        ListEmptyComponent={<Text>there are no recordings</Text>}
+      />
+    </View>
+  );
 };
 
 export default DataList;
