8f2b3e21a33aac24e929f2cdcfa74c7561a87306
flo <florian.eigendorf@tum.de>
2024-10-10 09:15:41 +0200

    add calculation of peaks and troughs by setting velocity equal to zero

--- services/AccelerometerProcessor.ts
+++ services/AccelerometerProcessor.ts
@@ -2,6 +2,7 @@ import { AccelerometerItem, DataPoint } from "../models";
 import WaveHeightFilter from "./WaveHeightFilter";
 import FourierTransform from "./FourierTransform";
 import HighPassFilter from "./HighPassFilter";
+import { mean } from "mathjs";
 
 var downsampler = require("downsample-lttb");
 class AccelerometerProcessor {
@@ -15,6 +16,9 @@ class AccelerometerProcessor {
 
   public reset(items: AccelerometerItem[]) {
     this.result = items.map((item) => [item.z]); // [accZ]
+
+    this._peaksIndex = undefined;
+    this._troughsIndex = undefined;
   }
 
   public applyHighPassFilter() {
@@ -50,6 +54,62 @@ class AccelerometerProcessor {
       date: index * WaveHeightFilter.dT,
       value: position,
     }));
+  }
+  private _peaksIndex?: number[];
+
+  get peaksIndex(): number[] {
+    if (this._peaksIndex == undefined) {
+      const signs = this.result
+        .map(([cumsum, position, velocity]) => velocity)
+        .map((velocity) => (velocity < 0 ? -1 : 1));
+
+      const peaks = [];
+      for (let i = 1; i < signs.length; i++) {
+        if (signs[i] - signs[i - 1] < 0) peaks.push(i);
+      }
+      this._peaksIndex = peaks;
+    }
+    return this._peaksIndex;
+  }
+
+  get peaks(): DataPoint[] {
+    return this.peaksIndex.map((index) => ({
+      date: index * WaveHeightFilter.dT,
+      value: this.result[index][1],
+    }));
+  }
+  private _troughsIndex?: number[];
+
+  private get troughsIndex(): number[] {
+    if (this._troughsIndex == undefined) {
+      const signs = this.result
+        .map(([cumsum, position, velocity]) => velocity)
+        .map((velocity) => (velocity < 0 ? -1 : 1));
+
+      const troughs = [];
+      for (let i = 1; i < signs.length; i++) {
+        if (signs[i] - signs[i - 1] > 0) troughs.push(i);
+      }
+      this._troughsIndex = troughs;
+    }
+    return this._troughsIndex;
+  }
+
+  get troughs(): DataPoint[] {
+    return this.troughsIndex.map((index) => ({
+      date: index * WaveHeightFilter.dT,
+      value: this.result[index][1],
+    }));
+  }
+
+  get meanWaveHeight() {
+    const peakMean = mean(
+      this.peaksIndex.map((index) => this.result[index][1])
+    );
+    const troughMean = mean(
+      this.troughsIndex.map((index) => this.result[index][1])
+    );
+    return -troughMean + peakMean;
   }
 }
 
